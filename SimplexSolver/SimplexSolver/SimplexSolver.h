0b995c9709e5acfc197eeddab3c71eadf886ccab
wowlocal <mishanevazhno@gmail.com>
2017-08-18 00:33:20 +0300

    no message

---
+++ SimplexSolver/SimplexSolver/SimplexSolver.h
@@ -0,0 +1,13 @@
+//
+//  SimplexSolver.h
+//  SimplexSolver
+//
+//  Created by misha on 18/08/2017.
+//  Copyright © 2017 misha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimplexSolver : NSObject
+
+@end
---
+++ SimplexSolver/SimplexSolver/SimplexSolver.m
@@ -0,0 +1,13 @@
+//
+//  SimplexSolver.m
+//  SimplexSolver
+//
+//  Created by misha on 18/08/2017.
+//  Copyright © 2017 misha. All rights reserved.
+//
+
+#import "SimplexSolver.h"
+
+@implementation SimplexSolver
+
+@end
